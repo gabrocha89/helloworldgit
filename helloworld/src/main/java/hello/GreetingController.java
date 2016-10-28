711bca1c36a8ee4954e87593232427e15377580f
gabrocha89 <gsr.89@hotmail.com>
2016-10-28 17:26:38 -0200

    Estrutura novo projeto

---
+++ helloworld/src/main/java/hello/GreetingController.java
@@ -0,0 +1,24 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+    
+    
+/*    @RequestMapping("/olaMundoSpring")
+    public String execute() {
+        System.out.println("Executando a lógica com Spring MVC");
+        return "ok";
+    }*/
+
+}
